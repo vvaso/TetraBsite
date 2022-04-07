774ac408e47d744575943d056377792589488b84
vvaso <56593285+vvaso@users.noreply.github.com>
2022-04-07 15:56:24 +0300

    Add files via upload

---
+++ @client-app/server/api/config/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+  // DB params 
+  define('DB_HOST','localhost');
+  define('DB_USER','root');
+  define('DB_PASSWORD','');
+  define('DB_NAME','tetrabyte team');
+
+  // EMAIL 
+  define('ADMIN_EMAIL','a.malesija@mc-class.gr');
