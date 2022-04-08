9f442a0882309f0710f0c96d3d30be027762ffe7
vvaso <56593285+vvaso@users.noreply.github.com>
2022-04-08 09:49:10 +0300

    Delete @client-app/client directory

--- @client-app/client/src/pages/Not Found.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { notFound } from '../constant';
-
-
-const NotFound = () => (<img src={notFound} alt='' width='100%' height='370px' />);
-
-
-export { NotFound };
--- @client-app/client/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-
-export * from './About Creator';
-export * from './Booking';
-export * from './Contact';
-export * from './Gallery';
-export * from './Home';
-export * from './Search';
-export * from './Not Found';
